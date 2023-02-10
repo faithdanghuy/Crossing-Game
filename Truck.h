13fda5c25676b3565b86ac5c5f7022fd1006ee69
Đặng Hà Huy <98213736+faithdanghuy@users.noreply.github.com>
2023-02-10 20:48:19 +0700

    Delete Truck.h

--- Truck.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "Vehicle.h"
-
-class Truck : public Vehicle {
-public:
-	static map<DIRECTION, vector<vector<wstring>>> spriteSheet;
-	Truck(float x, float y, int min_x, int max_x, float speed, DIRECTION dir = DIRECTION::RIGHT);
-};
