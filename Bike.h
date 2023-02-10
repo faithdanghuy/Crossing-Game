89a806fa4c459195789bd63fea2e38bd2d9bb71c
Đặng Hà Huy <98213736+faithdanghuy@users.noreply.github.com>
2023-02-10 20:46:34 +0700

    Delete Bike.h

--- Bike.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Vehicle.h"
-
-class Bike : public Vehicle {
-public:
-	static map<DIRECTION, vector<vector<wstring>>> spriteSheet;
-	Bike(float x, float y, int min_x, int max_x, float speed, DIRECTION dir = DIRECTION::RIGHT);
-	~Bike();
-};
